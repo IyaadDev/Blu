921f6982446133376dd3efba14112adbc09cc980
IyaadDev <113124292+IyaadDev@users.noreply.github.com>
2023-05-11 12:08:33 +0400

    Stashing my commits

---
+++ start.py
@@ -0,0 +1,10 @@
+# How to interact with the API.
+# This is the demo file
+# In this demo, I'm going to show you how you can fetch an API key's account data.
+# This is the most basic thing you can do with the API.
+# You can also fetch the account data of other users, but you need their API key for that.
+# First, import requests.
+import requests
+# Then, send the request to "https://faas-blr1-8177d592.doserverless.co/api/v1/web/fn-3f654592-68ef-49a6-a44d-081ec4e1f4c5/default/fetchrow?yourapikey"
+# Replace "yourapikey" with your API key.
+# Currently, you cannot get your own API key,but you can u
